cf62bc32609d7fd35113962ee99f71d9ba8f7665
Jonathan Parnell <jonathan.parnell1@gmail.com>
2018-04-13 20:09:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,5 @@ var playlist = {
   'Alan Jackson': 'Little Bity'
 }
 function updatePlayList(playlist, artistName, songTitle){
-  return Object.assign({}, playlist, { ['Mozart']: 'Moonlight Sonata' })
+  return Object.assign({}, playlist['Slowdive'] = 'Alison' playlist['My Bloody Valentine'] = 'Sometimes'
 }
