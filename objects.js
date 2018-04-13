a1e1aa1005f6f007ea32dc040e4ffe2388424cca
Jonathan Parnell <jonathan.parnell1@gmail.com>
2018-04-13 20:19:36 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist = {
 'Slowdive': 'Alison',
 'My Bloody Valentine': 'Sometimes'
 }
-function updatePlayList(obj, artistName, songTitle){
+function updatePlayList(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {['Phil Ochs']: "Here's to the State of Mississippi"});
 }
