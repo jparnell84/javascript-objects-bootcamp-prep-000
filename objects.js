5eeb147a7f45e11f3c8c9785ed834ea262d95d65
Jonathan Parnell <jonathan.parnell1@gmail.com>
2018-04-13 20:35:46 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist['Phil Ochs'] = "Here's to the State of Mississippi";
   return playlist;
 }
-function removeFromPlaylist(playlist, artistName, songTitle){
+function removeFromPlaylist(playlist, artistName){
   playlist['Phil Ochs'] = "Here's to the State of Mississippi";
   return playlist;
 }
